cea35d426c65ce8bacc8a225e3686885141ea794
Olivier Delalleau <507137+odelalleau@users.noreply.github.com>
2021-03-24 15:17:15 -0400

    Refactor: move functions around in built_in_resolvers.py
    No functional change

--- omegaconf/built_in_resolvers.py
+++ omegaconf/built_in_resolvers.py
@@ -8,41 +8,6 @@ from .errors import ValidationError
 from .grammar_parser import parse
 
 
-# DEPRECATED: remove in 2.2
-def legacy_env(key: str, default: Optional[str] = None) -> Any:
-    warnings.warn(
-        "The `env` resolver is deprecated, see https://github.com/omry/omegaconf/issues/573",
-    )
-
-    try:
-        return decode_primitive(os.environ[key])
-    except KeyError:
-        if default is not None:
-            return decode_primitive(default)
-        else:
-            raise ValidationError(f"Environment variable '{key}' not found")
-
-
-def env(key: str, default: Optional[str] = _DEFAULT_MARKER_) -> Optional[str]:
-    if (
-        default is not _DEFAULT_MARKER_
-        and default is not None
-        and not isinstance(default, str)
-    ):
-        raise TypeError(
-            f"The default value of the `oc.env` resolver must be a string or "
-            f"None, but `{default}` is of type {type(default).__name__}"
-        )
-
-    try:
-        return os.environ[key]
-    except KeyError:
-        if default is not _DEFAULT_MARKER_:
-            return default
-        else:
-            raise KeyError(f"Environment variable '{key}' not found")
-
-
 def decode(expr: Optional[str], _parent_: Container) -> Any:
     """
     Parse and evaluate `expr` according to the `singleElement` rule of the grammar.
@@ -69,3 +34,38 @@ def dict_keys(in_dict: Dict[Any, Any]) -> List[Any]:
 
 def dict_values(in_dict: Dict[Any, Any]) -> List[Any]:
     return list(in_dict.values())
+
+
+def env(key: str, default: Optional[str] = _DEFAULT_MARKER_) -> Optional[str]:
+    if (
+        default is not _DEFAULT_MARKER_
+        and default is not None
+        and not isinstance(default, str)
+    ):
+        raise TypeError(
+            f"The default value of the `oc.env` resolver must be a string or "
+            f"None, but `{default}` is of type {type(default).__name__}"
+        )
+
+    try:
+        return os.environ[key]
+    except KeyError:
+        if default is not _DEFAULT_MARKER_:
+            return default
+        else:
+            raise KeyError(f"Environment variable '{key}' not found")
+
+
+# DEPRECATED: remove in 2.2
+def legacy_env(key: str, default: Optional[str] = None) -> Any:
+    warnings.warn(
+        "The `env` resolver is deprecated, see https://github.com/omry/omegaconf/issues/573",
+    )
+
+    try:
+        return decode_primitive(os.environ[key])
+    except KeyError:
+        if default is not None:
+            return decode_primitive(default)
+        else:
+            raise ValidationError(f"Environment variable '{key}' not found")
