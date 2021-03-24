5fe304f12e39a68a49db0aa2ceafff6c156424da
Olivier Delalleau <507137+odelalleau@users.noreply.github.com>
2021-03-24 15:16:30 -0400

    Add new resolvers `oc.dict.keys` and `oc.dict.values`
    Fixes #643

--- omegaconf/built_in_resolvers.py
+++ omegaconf/built_in_resolvers.py
@@ -1,6 +1,6 @@
 import os
 import warnings
-from typing import Any, Optional
+from typing import Any, Dict, List, Optional
 
 from ._utils import _DEFAULT_MARKER_, _get_value, decode_primitive
 from .base import Container
@@ -61,3 +61,11 @@ def decode(expr: Optional[str], _parent_: Container) -> Any:
     parse_tree = parse(expr, parser_rule="singleElement", lexer_mode="VALUE_MODE")
     val = _parent_.resolve_parse_tree(parse_tree)
     return _get_value(val)
+
+
+def dict_keys(in_dict: Dict[Any, Any]) -> List[Any]:
+    return list(in_dict.keys())
+
+
+def dict_values(in_dict: Dict[Any, Any]) -> List[Any]:
+    return list(in_dict.values())
