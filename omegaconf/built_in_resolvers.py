9698fb8b64d91e2cd07d6a0e3979ad219f8e915d
Olivier Delalleau <507137+odelalleau@users.noreply.github.com>
2021-03-24 13:41:46 -0400

    Refactor: move built-in resolvers to built_in_resolvers.py
    No functional change

---
+++ omegaconf/built_in_resolvers.py
@@ -0,0 +1,63 @@
+import os
+import warnings
+from typing import Any, Optional
+
+from ._utils import _DEFAULT_MARKER_, _get_value, decode_primitive
+from .base import Container
+from .errors import ValidationError
+from .grammar_parser import parse
+
+
+# DEPRECATED: remove in 2.2
+def legacy_env(key: str, default: Optional[str] = None) -> Any:
+    warnings.warn(
+        "The `env` resolver is deprecated, see https://github.com/omry/omegaconf/issues/573",
+    )
+
+    try:
+        return decode_primitive(os.environ[key])
+    except KeyError:
+        if default is not None:
+            return decode_primitive(default)
+        else:
+            raise ValidationError(f"Environment variable '{key}' not found")
+
+
+def env(key: str, default: Optional[str] = _DEFAULT_MARKER_) -> Optional[str]:
+    if (
+        default is not _DEFAULT_MARKER_
+        and default is not None
+        and not isinstance(default, str)
+    ):
+        raise TypeError(
+            f"The default value of the `oc.env` resolver must be a string or "
+            f"None, but `{default}` is of type {type(default).__name__}"
+        )
+
+    try:
+        return os.environ[key]
+    except KeyError:
+        if default is not _DEFAULT_MARKER_:
+            return default
+        else:
+            raise KeyError(f"Environment variable '{key}' not found")
+
+
+def decode(expr: Optional[str], _parent_: Container) -> Any:
+    """
+    Parse and evaluate `expr` according to the `singleElement` rule of the grammar.
+
+    If `expr` is `None`, then return `None`.
+    """
+    if expr is None:
+        return None
+
+    if not isinstance(expr, str):
+        raise TypeError(
+            f"`oc.decode` can only take strings or None as input, "
+            f"but `{expr}` is of type {type(expr).__name__}"
+        )
+
+    parse_tree = parse(expr, parser_rule="singleElement", lexer_mode="VALUE_MODE")
+    val = _parent_.resolve_parse_tree(parse_tree)
+    return _get_value(val)
--- omegaconf/omegaconf.py
+++ omegaconf/omegaconf.py
@@ -32,7 +32,6 @@ from ._utils import (
     _ensure_container,
     _get_value,
     _is_none,
-    decode_primitive,
     format_and_raise,
     get_dict_key_value_types,
     get_list_element_type,
@@ -59,7 +58,6 @@ from .errors import (
     UnsupportedInterpolationType,
     ValidationError,
 )
-from .grammar_parser import parse
 from .nodes import (
     AnyNode,
     BooleanNode,
@@ -94,57 +92,7 @@ def SI(interpolation: str) -> Any:
 
 
 def register_default_resolvers() -> None:
-    # DEPRECATED: remove in 2.2
-    def legacy_env(key: str, default: Optional[str] = None) -> Any:
-        warnings.warn(
-            "The `env` resolver is deprecated, see https://github.com/omry/omegaconf/issues/573",
-        )
-
-        try:
-            return decode_primitive(os.environ[key])
-        except KeyError:
-            if default is not None:
-                return decode_primitive(default)
-            else:
-                raise ValidationError(f"Environment variable '{key}' not found")
-
-    def env(key: str, default: Optional[str] = _DEFAULT_MARKER_) -> Optional[str]:
-        if (
-            default is not _DEFAULT_MARKER_
-            and default is not None
-            and not isinstance(default, str)
-        ):
-            raise TypeError(
-                f"The default value of the `oc.env` resolver must be a string or "
-                f"None, but `{default}` is of type {type(default).__name__}"
-            )
-
-        try:
-            return os.environ[key]
-        except KeyError:
-            if default is not _DEFAULT_MARKER_:
-                return default
-            else:
-                raise KeyError(f"Environment variable '{key}' not found")
-
-    def decode(expr: Optional[str], _parent_: Container) -> Any:
-        """
-        Parse and evaluate `expr` according to the `singleElement` rule of the grammar.
-
-        If `expr` is `None`, then return `None`.
-        """
-        if expr is None:
-            return None
-
-        if not isinstance(expr, str):
-            raise TypeError(
-                f"`oc.decode` can only take strings or None as input, "
-                f"but `{expr}` is of type {type(expr).__name__}"
-            )
-
-        parse_tree = parse(expr, parser_rule="singleElement", lexer_mode="VALUE_MODE")
-        val = _parent_.resolve_parse_tree(parse_tree)
-        return _get_value(val)
+    from .built_in_resolvers import decode, env, legacy_env
 
     OmegaConf.legacy_register_resolver("env", legacy_env)
     OmegaConf.register_new_resolver("oc.env", env, use_cache=False)
